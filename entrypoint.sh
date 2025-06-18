bf3414723861765357015d589aa9465468149eb4
Jacob Yu <jayu@redhat.com>
2025-06-19 01:05:34 +0900

    Update entrypoint.sh  20250619

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,28 @@ do
    wait $!
    instruction=$(shuf -i 0-4 -n 1)
    d=`date -Iseconds`
+   JSON=$(jq -n -c '{
+   "timestamp":"2025-06-17T11:41:47+09:00",
+   "host":"reg-bkkkw0-001.apps.reg-mcp-001.xlcp.p1.openshiftapps.com.awsres.local",
+   "status":"200",
+   "remote_addr":"127.0.0.6",
+   "remote_user":"",
+   "request_method":"GET",
+   "request_uri":"/event/v1/socket/?token=04C50EBB-CF75-4E61-98EF-3BA65B8927E8&EIO=4&transport=polling&t=PTxmv-e",
+   "server_protocol":"HTTP/1.1",
+   "body_bytes_sent":"118",
+   "request_time":"0.001",
+   "upstream_response_time":"0.001",
+   "http_referer":"https://reg-bkkkw0-001.apps.reg-mcp-001.xlcp.p1.openshiftapps.com.awsres.local/business/61075874/preparing/additional",
+   "http_user_agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0",
+   "http_x_forwarded_for":"10.128.64.8",
+   "http_x_forwarded_proto":"https"
+}')
+   
    case "$instruction" in
       "1") echo {\"level\":\"info\",\"ts\":\"2025-05-14T11:21:37.353Z\",\"logger\":\"controllers.GatewayCluster\",\"msg\":\"Cluster Status\",\"gatewaycluster"\":{\"name\":\"gwv6\",\"namespace\":\"xxxx\"},\"Condition:\":\"DeploymentReady\"}"
       ;;
-      "2") echo "{\"@timestamp\": \"$d\", \"level\": \"INFO\", \"message\": \"takes the value and converts it to string.\"}"
+      "2") echo "$JSON"
       ;;
       "3") echo "{\"@timestamp\": \"$d\", \"level\": \"WARN\", \"message2\": \"Jacob test to see if this can all be included.\"}"
       ;;
