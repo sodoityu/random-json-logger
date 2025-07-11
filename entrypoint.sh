abac42a9c1adebd70338b80c82e9e504aff838c0
Jacob Yu <jayu@redhat.com>
2025-07-11 14:26:23 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ do
       ;;
       "2") echo "$JSON"
       ;;
-      "3") echo "{\"@timestamp\": \"$d\", \"level\": \"WARN\", \"message2\": \"Jacob test to see if this can all be included.\"}"
+      "3") echo "{\"@timestamp\": \"$d\", \"￥level\": \"WARN\", \"message2\": \"Jacob test to see if this can all be included.\"}"
       ;;
       "4") echo "{\"type\":\"liberty_message\",\"host\":\"xxxx\",\"ibm_userDir\":\"\/opt\/ibm\/wlp\/usr\/\",\"ibm_serverName\":\"defaultServer\",\"message\":\"sql_log 2025-05-09 11:33:04.348 SQL xxxx Default Executor-thread-425|559 xxxxxxx preparedStatement: jp.xxxx.wrapper.ConnectionWrapper@5bbbb63e: jp.xxxx.jdbc.internal.wrapper.PreparedStatementWrapper@xxxx:        SELECT      aaaa     ,xxxx    FROM     (      (      SELECT         1       ,aaa AS aaa       ,bbb AS bbb      FROM       ccc.ccc      WHERE       ddd >= 100      ORDER BY        bbb)      UNION ALL      (      SELECT        2       ,ccc AS ccc       ,lll AS lll      FROM       aaa.aaa      WHERE       ddd < 100      ORDER BY       sdn_list_id      )     ORDER BY      1     )      --aaaa\",\"ibm_threadId\":\"0000022f\",\"ibm_datetime\":\"2025-05-09T11:33:04.348+0900\",\"module\":\"SystemOut\",\"loglevel\":\"SystemOut\",\"ibm_methodName\":\"\",\"ibm_className\":\"\",\"ibm_sequence\":\"1746757984348_0000000006B66\",\"ext_appName\":\"aaaaa\",\"ext_thread\":\"Default Executor-thread-425\"}"
       ;;
